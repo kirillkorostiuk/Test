5ecf72a5a7b45c7ec9de8635feb0e2578bbe981b
kirillkorostiuk <103371335+kirillkorostiuk@users.noreply.github.com>
2022-09-14 10:52:56 +0300

    update index1

--- index1.js
+++ index1.js
@@ -1,3 +1,5 @@
-function moneyConverter (Number, price) {
-  document.getElementById("outputBTC").innerHTML=Number*1/price;
+function moneyConverter () { 
+  const dol = document.getElementById('inputdol').value
+  const price = document.getElementById('inputPrice').value
+  document.getElementById('outputBTC').innerHTML = dol*1/price
 }
